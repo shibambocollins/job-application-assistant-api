d8212aa908bfeccfaf61da161485f12ce057baa8
shibambocollins <ntsobokwanec@gmail.com>
2026-06-19 05:16:29 +0200

    Add Skill model to represent skills with name attribute

---
+++ src/main/java/za/ac/cput/jobassistantapi/model/Skill.java
@@ -0,0 +1,20 @@
+package za.ac.cput.jobassistantapi.model;
+
+public class Skill {
+
+    private String name;
+
+    public Skill() {}
+
+    public Skill(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
